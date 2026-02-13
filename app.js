6b3646df01f7e916ab6983339720fa8be6b9ca0f
muhammadnurfaizi <faiz130201@gmail.com>
2026-02-13 20:54:53 +0700

    install ejs, setting ejs, pisah view

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Hello Word");
+const express   = require('express')
+const app       = express()
+const port      = 3000
+
+app.set("view engine", "ejs") // setiing penggunaan template engine untuk express
+app.set("views", "./view") // setting pengunaan folder untuk menyompan file .eks
+
+app.get('/', (req, res) => {
+  res.render
+    (`branda`)
+})
+
+app.get('/profile', (req, res) => {
+  res.render
+    (`profile`)
+})
+
+
+app.listen(port, () => {
+  console.log(`Applikasi berjalan di http://localhost: ${port}`)
+})
