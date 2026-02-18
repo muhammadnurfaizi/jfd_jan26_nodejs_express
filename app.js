7ab48d20fa0df01744a30620b1584917a71f8fe8
muhammadnurfaizi <faiz130201@gmail.com>
2026-02-18 19:59:47 +0700

    form tambah & cara ambil isi form di back-end

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express   = require('express')
+const { log, profile } = require('node:console')
 const app       = express()
 const port      = 3000
 
+app.use( express.urlencoded({extended:false}))
 app.set("view engine", "ejs") // setiing penggunaan template engine untuk express
 app.set("views", "./view") // setting pengunaan folder untuk menyompan file .eks
 
@@ -26,6 +28,39 @@ app.get("/pengalaman", (req, res) => {
         usia: 25,
         gaji: 11000000
 })
+})
+
+// Synchronous = berjalan berurutan
+// asynchrounus = berjalan tidak berurutan
+
+app.get(`/karyawan`, async (req,res)=>{
+  res.render(`karyawan/all`, {
+    data_karyawan: await require(`./model/m_karyawan`).get_semua_karyawan()
+  })
+})
+
+app.get(`/karyawan/detail/:id_kry`, async (req, res)=>{
+  let id_kry = req.params.id_kry
+  res.render(`karyawan/profile`, {
+    Profile_Karyawan: await require(`./model/m_karyawan`).get_1_karyawan(id_kry)
+  })
+})
+
+app.get(`/karyawan/hapus/:id_kry`, async (req, res)=>{
+  let id_kry = req.params.id_kry
+  let proses_hapus =  await require(`./model/m_karyawan`).Hapus_1_karyawan(id_kry)
+  if (proses_hapus.effectedRows > 0 ){
+  res.redirect(`/karyawan`)
+  }
+
+})
+
+app.get(`/karyawan/tambah`,(req, res)=>{
+  res.render(`karyawan/form_tambah`)
+})
+
+app.post(`/karyawan/proses-insert`,(req, res)=>{
+  res.send(req.body)
 })
 
 app.listen(port, () => {
